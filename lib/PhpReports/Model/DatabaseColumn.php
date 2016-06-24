b9edca5d9ef798114a542fff0646270b39c9b3da
Paul <paul@yard.nl>
2016-06-24 10:12:48 +0200

    Regression is fixed, the non-generated models are now available again

---
+++ lib/PhpReports/Model/DatabaseColumn.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PhpReports\Model;
+
+use PhpReports\Model\Base\DatabaseColumn as BaseDatabaseColumn;
+
+/**
+ * Skeleton subclass for representing a row from the 'database_column' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class DatabaseColumn extends BaseDatabaseColumn
+{
+
+}
---
+++ lib/PhpReports/Model/DatabaseTable.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace PhpReports\Model;
+
+use PhpReports\Model\Base\DatabaseTable as BaseDatabaseTable;
+
+/**
+ * Skeleton subclass for representing a row from the 'database_table' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class DatabaseTable extends BaseDatabaseTable
+{
+
+}
